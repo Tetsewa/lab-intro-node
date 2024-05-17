78ec98db48a8d0447d0fd811cdb3603f3bd1b175
Tetsewa <amatwilliams@gmail.com>
2024-05-17 04:57:57 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    
+    this.items.push(item)
+    this.items.sort((a,b) => a - b)
+    this.length = this.items.length
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
 
-  min() {}
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+        }
+    return this.items[pos]
 
-  sum() {}
 
-  avg() {}
+  }
+
+  max() {
+
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items)
+
+  }
+
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items)
+  }
+
+  sum() {
+
+    return this.items.reduce((acc, curr) => acc+curr,0)
+
+  }
+
+  avg() {
+
+    if ( this.items.length === 0){
+      throw new Error("EmptySortedList");    }
+    else{
+      return this.sum()/this.items.length}
+
+  }
 }
 
 module.exports = SortedList;
